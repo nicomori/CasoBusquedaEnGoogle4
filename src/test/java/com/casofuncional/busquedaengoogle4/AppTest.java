4f99e7cef839ce248ab69e1bdf1ab9bec5758e67
nicomori <nicolas.mori@gmail.com>
2015-05-15 11:21:59 -0300

    Primer commit del ejemplo basico de busqueda en google

---
+++ src/test/java/com/casofuncional/busquedaengoogle4/AppTest.java
@@ -0,0 +1,43 @@
+package com.casofuncional.busquedaengoogle4;
+
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AppTest {
+
+	private FirefoxDriver elDriver;
+
+	@Before
+	public void before() {
+		elDriver = new FirefoxDriver();
+	}
+
+	@Test
+	public void test() {
+		elDriver.get("https://www.google.com.ar/");
+
+		elDriver.findElement(By.id("lst-ib")).sendKeys("www.automationcenter.com.ar");
+		elDriver.findElement(By.name("btnG")).click();
+		
+		WebElement myDynamicElement = (new WebDriverWait(elDriver, 10))
+				  .until(ExpectedConditions.presenceOfElementLocated(By.className("med")));
+	
+		Assert.assertTrue(elDriver.findElement(By.className("med")).isEnabled());
+		
+		
+	}
+	
+	@After
+	public void afterClass() {
+		elDriver.quit();
+	}
+	
+}
